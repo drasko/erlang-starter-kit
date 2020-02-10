a67009172f693887581e52f04a7895e7a037d2c3
Drasko Draskovic <drasko.draskovic@gmail.com>
2020-02-10 03:11:37 +0100

    Add logging
    Signed-off-by: Drasko Draskovic <drasko.draskovic@gmail.com>

--- src/esk_status.erl
+++ src/esk_status.erl
@@ -2,7 +2,10 @@
 
 -export([init/2]).
 
+-include_lib("kernel/include/logger.hrl").
+
 init(Req, Opts) ->
+    ?LOG_NOTICE(#{req => Req}),
     Resp = cowboy_req:reply(200,
         #{<<"content-type">> => <<"application/json">>},
         jsone:encode([{<<"status">>,<<"running">>}]),
